bd8732010f72d5413300830787a0150520ef2db4
HARINISRI M <harinisrim.ee25@bitsathy.ac.in>
2025-11-22 04:48:43 +0000

    remove uprgrade to pro from navigation bar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -34,13 +34,6 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from '@/components/ui/collapsible';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import React from 'react';
 import { cn } from '@/lib/utils';
 
@@ -156,22 +149,6 @@ export default function DashboardLayout({
               </Link>
             </nav>
           </div>
-          {isSidebarOpen && <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
-          </div>}
         </div>
       </div>
       <div className="flex flex-col">
@@ -266,22 +243,6 @@ export default function DashboardLayout({
                   Settings
                 </Link>
               </nav>
-              <div className="mt-auto">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Upgrade to Pro</CardTitle>
-                    <CardDescription>
-                      Unlock all features and get unlimited access to our
-                      support team.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Button size="sm" className="w-full">
-                      Upgrade
-                    </Button>
-                  </CardContent>
-                </Card>
-              </div>
             </SheetContent>
           </Sheet>
           <div className="w-full flex-1">
